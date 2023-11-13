840043141755ac0c667141e44479828aa463f231
anthony-gudiel <adg15@sfu.ca>
2023-11-12 22:28:56 -0800

    Added routing for home, content, resources, contact, about, as well as header

---
+++ app/src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='Home'>
+      <div className='landingTitle'>
+        <p>Home</p>
+      </div>
+    </div>
+  )
+}
