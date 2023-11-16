06f6d91426586afdd650c52d4cffdd68b8281abb
chl55 <chl55@sfu.ca>
2023-11-16 05:30:09 -0800

    Added vitest dependency and a placeholder test case

---
+++ app/test/App.test.jsx
@@ -0,0 +1,13 @@
+// App.test.jsx
+// Currently, the test cases are placeholders to ensure that the test suite is working properly.
+
+import { expect, test } from 'vitest'
+
+test('adds 1 + 2 to equal 3', () => {
+    const num1 = 1;
+    const num2 = 2;
+
+    const result = num1 + num2;
+
+    expect(result).toBe(3)
+})
