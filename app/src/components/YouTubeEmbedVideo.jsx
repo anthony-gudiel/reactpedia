73a0d6719e44e4bce0b9655a64607420626a7885
deannguyen1910 <deannguyen1910@gmail.com>
2023-11-16 08:07:14 -0800

    Add: feature: embed video after searching

---
+++ app/src/components/YouTubeEmbedVideo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const YoutubeEmbed = ({ embedId, width, height }) => (
+  <div className="video-responsive">
+    <iframe
+      width={width}
+      height={height}
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
+
+export default YoutubeEmbed;
