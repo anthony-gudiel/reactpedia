6c551769dc4aefa340ff18280d577a70e31d4a80
deannguyen1910 <deannguyen1910@gmail.com>
2023-11-16 08:02:48 -0800

    Add: feature - embed video after searching

--- app/src/pages/content/content.jsx
+++ app/src/pages/content/content.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import youtube from '../../api/youtube'
-import Search from '../../components/Search'
+import React, { useState } from 'react';
+import youtube from '../../api/youtube';
+import Search from '../../components/Search';
+import YoutubeEmbed from '../../components/YouTubeEmbedVideo';
+
 export const Content = () => {
-  const state= {
-    videos:[],
-    videoId: null
-  }
+  const [state, setState] = useState({
+    videos: [],
+    videoId: null,
+  });
 
-   const onSearch = async keyword => {
-    const response = await youtube.get("/search", {
-      params:{
-        q: keyword
-      }
+  const onSearch = async (keyword) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: keyword,
+      },
     });
-    
-    console.log(response);
-    
+
+    const embedId = response.data.items[0].id.videoId;
+
+    setState(() => ({
+      videos: response.data.items,
+      videoId: embedId,
+    }));
   }
 
-  
-    return(
-      <div className='App'>
+  return (
+    <div className="App">
       Content
-      <Search onSearch={onSearch}/>
-  
-      
-    </div>)
-
-}
+      <Search onSearch={onSearch} />
+      <YoutubeEmbed embedId={state.videoId} width="560" height="315" />
+    </div>
+  );
+};
