27d64908b3f69ba903a0f42ad9b957e1e0d84126
Jxgill2121 <144986259+Jxgill2121@users.noreply.github.com>
2023-11-24 01:49:01 -0800

    In web compiler attempt (#85)
    * One Compiler embedded Window
    
    * One Compiler embedded Window
    
    * Update contact.jsx
    
    ---------
    
    Co-authored-by: chl55 <144713450+chl55@users.noreply.github.com>

--- app/src/pages/contact/contact.jsx
+++ app/src/pages/contact/contact.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import CompilerIframe from '../../CompilerIFrame';
+import './contact.css';
 
 export const Contact = () => {
   return (
-    <div>
-      Contact
+    <div className="contact-container">
+      <h2 className="compiler-heading">Compiler</h2> {/* Heading with class */}
+      <p className="compiler-description">
+        This compiler is an excellent tool for practicing and learning ReactJS, JavaScript, and other programming languages. It provides an interactive environment where you can write, run, and test code in real-time. Whether you're working on small snippets or larger projects, this tool can greatly enhance your understanding and proficiency in coding. It's particularly useful for experimenting with new concepts and debugging.
+      </p>
+      <CompilerIframe 
+        src="https://onecompiler.com/react" 
+        width="1000" 
+        height="600" 
+      />
     </div>
   )
 }
