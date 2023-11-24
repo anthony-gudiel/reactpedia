3982d3318dc7dde5db461758c157e98c8936de8d
anthony-gudiel <adg15@sfu.ca>
2023-11-23 20:10:58 -0800

    Added third feature in lessons

--- app/src/api/openai.jsx
+++ app/src/api/openai.jsx
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 
-const openai = new OpenAI({ apiKey: 'key-goes-here' , dangerouslyAllowBrowser: true});
+const openai = new OpenAI({ apiKey: 'my-key' , dangerouslyAllowBrowser: true});
 
 export async function OPENAI(userMessage) {
   const completion = await openai.chat.completions.create({
@@ -11,4 +11,15 @@ export async function OPENAI(userMessage) {
   });
 
   return completion.choices[0].message.content;
+}
+
+export async function suggestedOPENAI(userMessage) {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: "You will provide related questions based on the user input" },
+      { role: "user", content:  userMessage }
+    ],    model: "gpt-3.5-turbo",
+  });
+
+  return completion.choices.map(choice => choice.message.content);
 }
